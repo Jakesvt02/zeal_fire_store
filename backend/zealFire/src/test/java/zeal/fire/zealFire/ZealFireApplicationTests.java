6d6bedc8e362e89865014f9a604af842d483efaa
Jacovt02 <jacovt02@gmail.com>
2024-11-18 17:08:43 +0200

    init

---
+++ backend/zealFire/src/test/java/zeal/fire/zealFire/ZealFireApplicationTests.java
@@ -0,0 +1,13 @@
+package zeal.fire.zealFire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZealFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
